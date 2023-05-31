411cbbb33b13f5faa685299ab4ad03b13e8788d3
xiaohei <xiaohei6856@qq.com>
2023-05-31 10:38:52 +0800

    项目初始化，全局引入组件TDesign，Vue3+Vuex+sass+bable+router，删除无关内容

---
+++ breeding/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import TDesign from 'tdesign-vue-next';
+import router from './router'
+import store from './store'
+// 引入组件库的少量全局样式变量
+import 'tdesign-vue-next/es/style/index.css';
+
+createApp(App).use(store).use(TDesign).use(router).mount('#app')
---
+++ breeding/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    首页
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
