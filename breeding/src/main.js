0460197ce2369d42bdd8dbeb025654e73fa8405f
wx <2713193934@qq.com>
2023-06-02 15:25:49 +0800

    侧边框样式

--- breeding/src/main.js
+++ breeding/src/main.js
@@ -5,5 +5,7 @@ import router from './router'
 import store from './store'
 // 引入组件库的少量全局样式变量
 import 'tdesign-vue-next/es/style/index.css';
+// import { registerIconComponent } from 'tdesign-icons-vue-next'
+// registerIconComponent(app)
 
 createApp(App).use(store).use(TDesign).use(router).mount('#app')
