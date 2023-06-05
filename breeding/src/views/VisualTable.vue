3ce5c6c9ef70cba17b8af3f6a3d862a9b4d68ca6
xiaohei <xiaohei6856@qq.com>
2023-06-06 01:11:40 +0800

    新增登录注册页面，axios封装完成

--- breeding/src/views/VisualTable.vue
+++ breeding/src/views/VisualTable.vue
@@ -2,7 +2,7 @@
   <div>
       可视化
       i'm xiaohuihui
-        <div style="height:1900px;background-color:#000">asdf</div>
+        <div style="height:1900px;background-color:#fff">asdf</div>
   </div>
 </template>
 
