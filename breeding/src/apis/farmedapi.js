01b03b5d81401ca81a8484a4b0599ff44b116c59
wx <2713193934@qq.com>
2023-06-15 08:49:10 +0800

    养殖舍数据

---
+++ breeding/src/apis/farmedapi.js
@@ -0,0 +1,80 @@
+import { post } from "@/utils/axios";
+import { get } from "@/utils/axios";
+import { del } from "@/utils/axios";
+import { patch } from "@/utils/axios";
+
+/**
+ *  添加养殖舍
+ * @param {} data 
+ * @returns 
+ */
+export const farmAdd = function(data){
+    return post({
+        url:`/houses/add`,
+        data
+    })
+}
+/**
+ * 删除养殖舍
+ * @returns 
+ */
+export const farmDel = function(){
+    return del({
+        url:`/houses/delete`
+    })
+}
+/**
+ * 查找所有养殖舍
+ * @param {*} params 
+ * @returns 
+ */
+export const farmFindAll = function(params){
+    return get({
+        url:`/houses/findAllHouses`,
+        params
+    })
+}
+/**
+ * 根据种类查找养殖舍
+ * @param {} params 
+ * @returns 
+ */
+export const farmFindByType = function(params){
+    return get({
+        url:`/houses/findByType`,
+        params
+    })
+}
+/**
+ * 养殖舍类型
+ * @param {*} params 
+ * @returns 
+ */
+export const farmGetTypes = function(params){
+    return get({
+        url:`/houses/getTypes`,
+        params
+    })
+}
+/**
+ * houseModel
+ * @param {} data 
+ * @returns 
+ */
+export const houseModel = function(data){
+    return post({
+        url:`/houses/houseModel`,
+        data
+    })
+}
+/**
+ * 修改养殖舍
+ * @param {*} data 
+ * @returns 
+ */
+export const farmModify = function(data){
+    return patch({
+        url:`/houses/modify`,
+        data
+    })
+}
